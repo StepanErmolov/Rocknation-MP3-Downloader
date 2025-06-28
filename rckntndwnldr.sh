e0e9b283be17faf64ae02558a46cceab3ff76240
StepanErmolov <163306635+StepanErmolov@users.noreply.github.com>
2025-06-28 16:53:45 +0300

    Update and rename downloader.sh to rckntndwnldr.sh

--- rckntndwnldr.sh
+++ rckntndwnldr.sh
@@ -12,7 +12,11 @@ function download() {
 
 function download_from_page() {
 	curl "$1" -o /tmp/rckntn.html
+ 	dir=$(sed -n 's|.*<title>\(.*\) listen online слушать онлайн mp3 download скачать rocknation.su</title>.*|\1|p' /tmp/rckntn.html)
+	mkdir "$dir"
+	cd "$dir"
 	download
+ 	cd ..
 }
 
 for arg; do
